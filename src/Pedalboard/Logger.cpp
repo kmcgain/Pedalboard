68d8248e42521d2acd75974d71e8072460815b02
kym.mcgain <kym.mcgain@ratesetter.com.au>
2021-03-21 13:31:37 +1100

    Use compiler directive to stop logging - for better performance in release mode

--- src/Pedalboard/Logger.cpp
+++ src/Pedalboard/Logger.cpp
@@ -9,55 +9,73 @@ int next = 0;
 // crash due to interrupts being unavailable (for serial IO)
 // We should only call flush from main loop outside atomic block
 void Logger::flush() {
+#ifdef LOGGING_ON	
 	for (int i = 0; i < next; i++)
 		Serial.print(pending[i]);
 	next = 0;
+#endif
 }
 
 void Logger::log(const char msg[]) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%s", msg);
+#endif
 }
 
 void Logger::log(char* msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%s", msg);
+#endif
 }
 
 void Logger::log(char msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%s", msg);
+#endif
 }
 
 void Logger::log(int msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%d", msg);
+#endif
 }
 
 void Logger::log(unsigned int msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%d", msg);
+#endif
 }
 
 void Logger::log(long msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%d", msg);
+#endif
 }
 
 void Logger::log(unsigned long msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%d", msg);
+#endif
 }
 
 void Logger::log(double msg) {
+#ifdef LOGGING_ON	
 	if (next >= SIZE)
 		return;
 	sprintf(pending[next++], "%d", msg);
+#endif
 }
